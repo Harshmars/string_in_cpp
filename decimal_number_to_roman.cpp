0cb1b1c9895c5bddc550a26be96f0187fce369e9
Harshmars <105042579+Harshmars@users.noreply.github.com>
2023-02-06 19:05:24 +0530

    Create decimal_number_to_roman.cpp

---
+++ decimal_number_to_roman.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int char_to_num(char ch){
+    switch(ch){
+        case 'v':
+        return 5;
+        case 'i':
+        return 1;
+        case 'x':
+        return 10;
+        case 'l':
+        return 50;
+        case 'c':
+        return 100;
+        case 'd':
+        return 500;
+        case 'm':
+        return 1000;
+    }
+}
+int main(){
+    string str;
+    cout<<"enter roman number :  ";
+    cin>>str;
+    int res=char_to_num(str[str.length()-1]);
+    for(int i=str.length()-2;i>=0;i--){
+        int num=char_to_num(str[i]);
+        if(num==res-char_to_num( str[i+1])){
+            res+=num;
+        }
+        else if(res>num){
+            res-=num;
+        }
+        
+        else{
+            res+=num;
+        }
+    }
+  cout<<res;
+  return 0;
+}
